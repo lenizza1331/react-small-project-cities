b8f06864ed5d659d4256ec6233ac70d860c4d96e
lenizza1331 <leniyuva@gmail.com>
2022-03-25 01:34:09 +0300

    commit one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+const [topCity, setTopCity ] = useState(data);
+
+const removeCity = (id)=>{
+  let newCity = topCity.filter(topCity=> topCity.id !== id);
+  setTopCity(newCity);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='center'>
+        <h1 className='title'>Top {topCity.length} largest cities in the world by population</h1>
+      </div>
+    {topCity.map((elem=>{
+      const {id, city, country, population, square, description, image} = elem;
+      return (
+        <div key={id}>
+        <div className='center title'>
+          <h2>{id} - {city}, {country}</h2>
+        </div>
+
+        <div className='center numbers'>
+        <h4>Population: {population} people</h4>
+        <p>The area of the city: {square} sq.m</p>
+        </div>
+
+        <div className='box '>
+        <img className='photos' src={image} alt="city photo"/>
+        </div>
+
+        <div className='box'>
+          <div className='center about'>
+            <p>{description}</p>
+          </div>
+        </div>
+
+
+        <div className='center btn'>
+          <button onClick={()=> removeCity(id)}>Remove</button>
+        </div>
+
+        <hr className='her'/>
+        </div>
+      )
+    }))}
+
+<div className='center delete'>
+  <button onClick={()=>setTopCity([])}>Delete all</button>
+</div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,108 @@
+export const data = [
+    {
+        id: 1,
+        city:"Tokyo" ,
+        country: "Japan" ,
+        population: "37 400 068" ,
+        square: "8 223" ,
+        description:"Tokyo is the largest urban agglomeration in the world. Greater Tokyo has 38.1 million people - almost the same number of people live in Canada (37.6 million). It is one of the 47 prefectures in Japan, consisting of 23 central urban areas and many cities, towns and villages west of the city center + the islands of Izu and Ogasawara are also considered part of Tokyo. According to UN forecasts, by 2030 the capital of Japan will move to second place in the ranking. ",
+        image:  "https://images.unsplash.com/photo-1513407030348-c983a97b98d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80" ,
+    },
+    {
+        id: 2 ,
+        city:"Delhi" ,
+        country:"India" ,
+        population:"28 514 000" ,
+        square:"2 240" ,
+        description:"The population is 28.5 million people, and according to forecasts, in 10 years the population will grow by another 10 million. Delhi is an important trade, transport, cultural, and political center of India. According to legend, the city was named after Raja Dilu, a king who ruled the region in the 1st century BC. e." ,
+        image: "https://images.pexels.com/photos/2402386/pexels-photo-2402386.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+            
+    },
+    
+    {
+        id: 3,
+        city: "Shanghai",
+        country:"China" ,
+        population:"25 582 000" ,
+        square:"4 015" ,
+        description:"Population - 25.5 million people. According to preliminary estimates, the population of Shanghai by 2030 will be 32.8 million people, which will allow it to remain in third place on the list. The territory of the municipality includes the city, suburbs and agricultural areas. Shanghai is the most populous city in China, and the municipality is its most populous part." ,
+        image: "https://images.unsplash.com/photo-1548919973-5cef591cdbc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+            
+    },
+    
+    {
+        id: 4,
+        city: "Sao Paulo",
+        country:"Brazil" ,
+        population:"21 650 000" ,
+        square: "3 043",
+        description:"21.6 million people live in the city. With a projected population of 23.8 million by 2030, the city will fall to 9th place on the list of the world's most populous cities. Sao Paulo is the largest city in the Southern Hemisphere and one of the largest metropolitan areas in the world." ,
+        image:  "https://images.unsplash.com/photo-1543059080-f9b1272213d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+            
+    },
+    
+    {
+        id: 5,
+        city: "Ciudad de Mexico",
+        country:"Mexico" ,
+        population: "21 581 000",
+        square: "2 370",
+        description:"The population is 21.5 million people, in 2030 Mexico City will still be in the top ten in terms of population, but only in 8th place. With a population of 24.1 million, it will become the largest city in the Western Hemisphere. Mexico City's leading position compared to other urban centers in the developing world can be attributed to its rich and diverse environment, long history, densely populated territory, and the central role that its rulers have assigned to it over the centuries. Centralism may have influenced the character of Mexico City the most, as the city has been a center of politics, religion, and commerce since the end of the Postclassic period (12th-16th century)." ,
+        image:  "https://images.unsplash.com/photo-1493794179168-82ca7cb00437?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+            
+    },
+    
+    {
+        id: 6,
+        city: "Cairo",
+        country:"Egypt" ,
+        population:"20 076 000" ,
+        square:"1 917" ,
+        description:"Population - 20 million people. Cairo, the largest Egyptian city for a thousand years, will become the fifth in the world by 2030. It is a fan-shaped city: narrow in the south, where the river valley is wedged between the slopes of the desert, and wide in the north, where the valley merges with the Nile Delta. Over the centuries, the city expanded to the west, but in response to increased demand, it began to expand to the north, south and west." ,
+        image:  "https://images.unsplash.com/photo-1562842523-6d492ddc3e25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+            
+    },
+    
+    {
+        id: 7,
+        city: "Mumbai",
+        country:"India" ,
+        population:"19 980 000" ,
+        square:"881" ,
+        description:"The population is 19.9 million people, by 2030 the expected population will be 24.5 million people. Located on the coast of Maharashtra, Mumbai is the most densely populated city in India, one of the largest urban areas in the world. It was built on the site of an ancient settlement and was named after the goddess Mumba, the incarnation of Parvati, the wife of Shiva, one of the main deities of Hinduism." ,
+        image: "https://images.unsplash.com/photo-1574565087163-11e46ca637a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1271&q=80",
+            
+    },
+    
+    {
+        id: 8,
+        city: "Beijing",
+        country:"China" ,
+        population:"19 618 000" ,
+        square:"4 144" ,
+        description: "The population is 19.6 million people. The UN predicts that Beijing will come in 7th place on the list with 24.2 million people by 2030. However, the country's population may begin to decline based on estimates of fertility and aging",
+        image: "https://images.unsplash.com/photo-1537265655479-901d3de699d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+            
+    },
+    
+    {
+        id: 9,
+        city: "Dhaka",
+        country:"Bangladesh" ,
+        population: "19 578 000",
+        square: "453" ,
+        description:"Bangladesh is in the top ten countries in the world in terms of population (19.5 million people), and Dhaka, its capital, may rise to the fourth position by 2030 with a projected population growth of almost 9 million people (reaching 28 million people) ." ,
+        image: "https://smapse.ru/image/full/name/10-samih-bolshih-gorodov-mira-smapse10.jpg/ ",
+            
+    },
+
+    {
+        id: 10,
+        city: "Osaka",
+        country:"Japan" ,
+        population:"19 281 000" ,
+        square:"3 004" ,
+        description:"19 million people live here. The city is projected to fall out of the list of the world's largest metropolitan areas as the country experiences negative population growth." ,
+        image:"https://images.unsplash.com/photo-1589452271712-64b8a66c7b71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+    },
+]
